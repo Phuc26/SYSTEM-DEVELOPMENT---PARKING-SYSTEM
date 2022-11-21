7ef89896626c8953b97520ce9267190394e81476
Alex Nguyen <2073215@edu.vaniercollege.qc.ca>
2022-11-21 08:40:41 -0500

    Swapped names of folders

--- UserWebApp/app/views/Main/index.php
+++ UserWebApp/app/views/Main/index.php
@@ -1,156 +1,22 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-   <head>
-      <meta charset="utf-8">
-      <title>VIP Valet</title>
-      <link rel="stylesheet" href="resources/styles/style.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-   </head>
-   <body>
-      <div class="container">
-         <img src="/resources/images/logo.png" alt="VIP Valet Logo">
-         <header>Reserve your parking</header>
-         <div class="progress-bar">
-            <div class="step">
-               <p>
-                  Plate
-               </p>
-               <div class="bullet">
-                  <span>1</span>
-               </div>
-               <div class="check fas fa-check"></div>
-            </div>
-            <div class="step">
-               <p>
-                  Location
-               </p>
-               <div class="bullet">
-                  <span>2</span>
-               </div>
-               <div class="check fas fa-check"></div>
-            </div>
-            <div class="step">
-               <p>
-                  Time
-               </p>
-               <div class="bullet">
-                  <span>3</span>
-               </div>
-               <div class="check fas fa-check"></div>
-            </div>
-            <div class="step">
-               <p>
-                  Receipt
-               </p>
-               <div class="bullet">
-                  <span>4</span>
-               </div>
-               <div class="check fas fa-check"></div>
-            </div>
-         </div>
-         <div class="form-outer">
-            <form action="#">
-               <div class="page slide-page">
-                  <div class="title">
-                     Car information:
-                  </div>
-                  <div class="field">
-                     <div class="label">
-                        License plate
-                     </div>
-                     <input type="text">
-                  </div>
-                  <!-- <div class="field">
-                     <div class="label">
-                        Last Name
-                     </div> 
-                     <input type="text">
-                  </div> -->
-                  <div class="field">
-                     <button class="firstNext next">Next</button>
-                  </div>
-               </div>
-               <div class="page">
-                  <div class="title">
-                     Location:
-                  </div>
-                  <!-- <div class="field">
-                     <div class="label">
-                        Email Address
-                     </div>
-                     <input type="text">
-                  </div>
-                  <div class="field">
-                     <div class="label">
-                        Phone Number
-                     </div>
-                     <input type="Number">
-                  </div> -->
-                  <div class="field">
-                    <div class="label">
-                       Parking location
-                    </div>
-                    <select>
-                        <option>--Choose--</option>
-                       <option>Laval</option>
-                       <option>Longeuil</option>
-                     <!--  <option>Other</option> -->
-                    </select>
-                 </div>
-                  <div class="field btns">
-                     <button class="prev-1 prev">Previous</button>
-                     <button class="next-1 next">Next</button>
-                  </div>
-               </div>
-               <div class="page">
-                  <div class="title">
-                     Parking duration
-                  </div>
-                  <div class="field">
-                     <div class="label">
-                        Today date
-                     </div>
-                     <!-- <input type="text"> -->
-                  </div>
-                  <!-- <div class="field">
-                     <div class="label">
-                        Gender
-                     </div>
-                     <select>
-                        <option>Male</option>
-                        <option>Female</option>
-                        <option>Other</option>
-                     </select>
-                  </div>  -->
-                  <div class="field btns">
-                     <button class="prev-2 prev">Previous</button>
-                     <button class="next-2 next">Next</button>
-                  </div>
-               </div>
-               <div class="page">
-                  <div class="title">
-                     Parking confirmation:
-                  </div>
-                  <!-- <div class="field">
-                     <div class="label">
-                        Username
-                     </div>
-                     <input type="text">
-                  </div>
-                  <div class="field">
-                     <div class="label">
-                        Password
-                     </div>
-                     <input type="password">
-                  </div> -->
-                  <div class="field btns">
-                     <button class="prev-3 prev">Previous</button>
-                     <button class="submit">Pay</button>
-                  </div>
-               </div>
-            </form>
-         </div>
-      </div>
-      <script src="/resources/scripts/script.js"></script>
-   </body>
+<html>
+
+<head>
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<!-- JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+	<title>Home</title>
+
+</head>
+
+<body>
+
+	<div class='container'>
+		<h1>Home</h1>
+		<p>Welcome. This will be our web app.</p>
+	</div>
+
+</body>
+
 </html>
