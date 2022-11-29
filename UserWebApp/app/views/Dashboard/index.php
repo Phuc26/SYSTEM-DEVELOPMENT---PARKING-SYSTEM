f539e64a357e8549e536020c70cdd4a231d5e2f6
alexNcomp <alexbuinguyen@gmail.com>
2022-11-29 18:09:34 -0500

    Added Dashboard

--- UserWebApp/app/views/Dashboard/index.php
+++ UserWebApp/app/views/Dashboard/index.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, inital-scale=1, shrink-to-fit=no">
-		<title>Login</title>
+		<title>Dashboard</title>
 		<link rel="stylesheet" href="resources/styles/styles.css">
 
 		<!-- Bootstrap CSS -->
@@ -16,10 +16,88 @@
 
 		<div class="container">		
 			<h1>Dashboard</h1><hr>
-				
+			<div class="row justify-content-center">
+				<div class="col-lg-3 col-md-6 mb-4">
+					<div class="card"> 
+						<svg xmlns="http://www.w3.org/2000/svg" width="50%" height="50%" fill="currentColor" class="bi bi-p-square-fill mx-auto my-2" viewBox="0 0 16 16">
+							<path d="M8.27 8.074c.893 0 1.419-.545 1.419-1.488s-.526-1.482-1.42-1.482H6.778v2.97H8.27Z"/>
+							<path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm3.5 4.002h2.962C10.045 4.002 11 5.104 11 6.586c0 1.494-.967 2.578-2.55 2.578H6.784V12H5.5V4.002Z"/>
+						</svg>
+						<div class="card-body text-center">
+							<p class="card-text">Set up a parking lot</p>
+							<a href="#" class="stretched-link"></a>
+						</div>
+					</div>
+				</div>
 
-               		
-				</form>
+				<div class="col-lg-3 col-md-6 mb-4">
+					<div class="card"> 
+						<svg xmlns="http://www.w3.org/2000/svg" width="50%" height="50%" fill="currentColor" class="bi bi-eye-fill mx-auto my-2" viewBox="0 0 16 16">
+  							<path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+  							<path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+						</svg>
+						<div class="card-body text-center">
+							<p class="card-text">View Parking Lot Details</p>
+							<a href="#" class="stretched-link"></a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 mb-4">
+					<div class="card"> 
+						<svg xmlns="http://www.w3.org/2000/svg" width="50%" height="50%" fill="currentColor" class="bi bi-arrow-clockwise mx-auto my-2" viewBox="0 0 16 16">
+							<path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+							<path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+						</svg>
+						<div class="card-body text-center">
+							<p class="card-text">Update Parking Lot Details</p>
+							<a href="#" class="stretched-link"></a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 mb-4">
+					<div class="card"> 
+						<svg xmlns="http://www.w3.org/2000/svg" width="50%" height="50%" fill="currentColor" class="bi bi-sign-stop-fill mx-auto my-2" viewBox="0 0 16 16">
+							<path d="M10.371 8.277v-.553c0-.827-.422-1.234-.987-1.234-.572 0-.99.407-.99 1.234v.553c0 .83.418 1.237.99 1.237.565 0 .987-.408.987-1.237Zm2.586-.24c.463 0 .735-.272.735-.744s-.272-.741-.735-.741h-.774v1.485h.774Z"/>
+							<path d="M4.893 0a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146A.5.5 0 0 0 11.107 0H4.893ZM3.16 10.08c-.931 0-1.447-.493-1.494-1.132h.653c.065.346.396.583.891.583.524 0 .83-.246.83-.62 0-.303-.203-.467-.637-.572l-.656-.164c-.61-.147-.978-.51-.978-1.078 0-.706.597-1.184 1.444-1.184.853 0 1.386.475 1.436 1.087h-.645c-.064-.32-.352-.542-.797-.542-.472 0-.77.246-.77.6 0 .261.196.437.553.522l.654.161c.673.164 1.06.487 1.06 1.11 0 .736-.574 1.228-1.544 1.228Zm3.427-3.51V10h-.665V6.57H4.753V6h3.006v.568H6.587Zm4.458 1.16v.544c0 1.131-.636 1.805-1.661 1.805-1.026 0-1.664-.674-1.664-1.805V7.73c0-1.136.638-1.807 1.664-1.807 1.025 0 1.66.674 1.66 1.807ZM11.52 6h1.535c.82 0 1.316.55 1.316 1.292 0 .747-.501 1.289-1.321 1.289h-.865V10h-.665V6.001Z"/>
+						</svg>
+						<div class="card-body text-center">
+							<p class="card-text">Disable Parking Lot</p>
+							<a href="#" class="stretched-link"></a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row justify-content-center mb-4"> 
+				<div class="col-lg-3 col-md-6 mb-4">
+					<div class="card"> 
+						<svg xmlns="http://www.w3.org/2000/svg" width="50%" height="50%" fill="currentColor" class="bi bi-currency-dollar mx-auto my-2" viewBox="0 0 16 16">
+						  	<path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
+						</svg>
+						<div class="card-body text-center">
+							<p class="card-text">Modify Parking Rates</p>
+							<a href="#" class="stretched-link"></a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-3 col-md-6 mb-4">
+					<div class="card"> 
+						<svg xmlns="http://www.w3.org/2000/svg" width="50%" height="50%" fill="currentColor" class="bi bi-cash-coin mx-auto my-2" viewBox="0 0 16 16">
+							<path fill-rule="evenodd" d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0z"/>
+							<path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1h-.003zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195l.054.012z"/>
+							<path d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083c.058-.344.145-.678.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1H1z"/>
+							<path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 5.982 5.982 0 0 1 3.13-1.567z"/>
+						</svg>
+						<div class="card-body text-center">
+							<p class="card-text">View Revenue</p>
+							<a href="#" class="stretched-link"></a>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 		
     	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
