503a39b8b8e9f452b5f49c07c6d3191adc10b7de
antho-mastro <anthony.mastronardi2@gmail.com>
2022-12-06 20:53:09 -0500

    paypal functional

--- ParkerWebApp/app/views/Main/index.php
+++ ParkerWebApp/app/views/Main/index.php
@@ -18,7 +18,7 @@
                <h3>Reserve Your Parking</h3> <hr>
             </div>
          
-         <form>
+         <form><input hidden id="totalPrice" name="totalPrice" value= "0">
             <div class="col-md mb-5">
                <h2 class="text-muted">License plate</h3>
                <input type="text" class="form-control" name="license" placeholder="1A1 B2B" required>
@@ -26,26 +26,119 @@
 
             <div class="col-md mb-5">
                <h2 class="text-muted">Parking lot</h3>
-               <select class="form-select" name="location" required>
+               <select class="form-select" name="location" id="location" required>
                   <option selected disabled>Choose your parking lot</option>
-                  <option value="laval">Laval</option>
-                  <option value="longueil">Longueil</option>
+                  <option value="1.15">Laval</option>
+                  <option value="1.5">Longueil</option>     
                </select>
             </div>
 
             <div class="col-md mb-5">
                <h2 class="text-muted">Duration</h3>
-               <input type="number" class="form-control" name="duration" min="1" max="24" placeholder="Select a time" required>
+               <input type="number" class="form-control" name="duration" min="1" max="24" id="duration" placeholder="Select a time" required>
+
             </div>
 
-            <button type="submit" name="action" class="btn btn-primary float-end mt-2 mb-4 mx-4">Confirm</button>
+            <div class="d-flex justify-content-center">
+               <span class="fs-3 text-muted">Total: $</span>
+               <strong>
+                  <span id="displayTotal" class="fs-3">
+
+               0.00 
+
+               
+            </span>
+               </strong>
+          </div>
+
+            <button id="button1" hidden type="submit" name="action" class="btn btn-primary float-end mt-2 mb-4 mx-4">Confirm</button>
          </form>
+         <div  id="smart-button-container">
+                      <div style='text-align: center;'>
+                        <div id='paypal-button-container'>
+                        </div>
+                      </div>
+                    </div>
       </div>
+
 
       <script src="/resources/scripts/scripts.js"></script>
 
       <!-- Bootstrap JS -->
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-   </body>
 
+      <script src="https://code.jquery.com/jquery-3.6.1.min.js"  integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+
+
+
+      <script>
+         $( "#location" ).change(function checkSelect() {
+      var rate = $("#location").val();
+      var hours = $("#duration").val();
+      var total = rate*hours;
+      $("#totalPrice").val(total.toFixed(2));
+      $("#displayTotal").html(total.toFixed(2));
+      });
+
+         $( "#duration" ).change(function checkInput() {
+      var rate = $("#location").val();
+      var hours = $("#duration").val();
+      var total = rate*hours;
+      $("#totalPrice").val(total.toFixed(2));
+      $("#displayTotal").html(total.toFixed(2));
+      });
+   </script>
+
+
+      <script src="https://www.paypal.com/sdk/js?client-id=Ad3WMY55vhrSZ1ekbMNTXRyyHDWwQwBDCVlhomIMIYDSTKhcmajW8q-fO1ahus5eGkp-eqsDAnDv8IwO&locale=en_US&commit=true&currency=CAD%22%3E"></script>
+         <script src="https://www.paypal.com/sdk/js?client-id=sb&enable-funding=venmo&currency=CAD" data-sdk-integration-source="button-factory"></script>
+
+
+  
+  <script>
+    function initPayPalButton() {
+      paypal.Buttons({
+        style: {
+          shape: 'pill',
+          color: 'gold',
+          layout: 'vertical',
+          label: 'checkout',
+
+        },
+
+        createOrder: function(data, actions) {
+          const amount =  document.getElementById('totalPrice').value;
+          return actions.order.create({
+            purchase_units: [{"amount":{"currency_code":"CAD","value":amount}}]
+          });
+        },
+
+        onApprove: function(data, actions) {
+          return actions.order.capture().then(function(orderData) {
+
+            // Full available details
+            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+
+            // Show a success message within this page, e.g.
+            const element = document.getElementById('paypal-button-container');
+            element.innerHTML = '';
+            element.innerHTML = '<h3>Thank you for your payment!</h3>';
+
+            $('#total_price').val($("#totalPrice").val());
+            // const order_id =  document.getElementById('order_id').value;
+            $( "#button1").click();
+            // $( "form#checkout_form" ).submit();
+            // Or go to another URL:  actions.redirect('thank_you.html');
+
+          });
+        },
+
+        onError: function(err) {
+          console.log(err);
+        }
+      }).render('#paypal-button-container');
+    }
+    initPayPalButton();
+      </script>
+   </body>
 </html>
