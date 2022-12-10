fd449dc3d9f64c349b16198224c0616c1147e726
alexNcomp <alexbuinguyen@gmail.com>
2022-12-09 19:20:28 -0500

    Added validation for parking

--- ParkerWebApp/app/controllers/Main.php
+++ ParkerWebApp/app/controllers/Main.php
@@ -3,11 +3,18 @@ namespace app\controllers;
 
 class Main extends \app\core\Controller {
 	
-	public function index(){	
-		$this->view('Main/index');
+	public function index() {
+		if (!isset($_POST['action'])) {
+			$this->view('Main/index');
+		} else {
+			$_SESSION['license'] 	= strtoupper($_POST['license']);
+			$_SESSION['location']	= $_POST['location'];
+			$_SESSION['duration']	= $_POST['duration'];
+			header("location:/Main/confirmation");
+		}
 	}
 
-	public function confirmation(){	
+	public function confirmation() {	
 		$this->view('Main/confirmation');
 	}
 }
