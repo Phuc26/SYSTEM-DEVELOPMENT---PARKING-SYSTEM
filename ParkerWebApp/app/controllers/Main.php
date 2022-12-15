a4b1e2adf868ea40305cc6f1bea06d73a7235af7
antho-mastro <anthony.mastronardi2@gmail.com>
2022-12-14 23:56:08 -0500

    Update Main.php

--- ParkerWebApp/app/controllers/Main.php
+++ ParkerWebApp/app/controllers/Main.php
@@ -4,17 +4,43 @@ namespace app\controllers;
 class Main extends \app\core\Controller {
 	
 	public function index() {
-		if (!isset($_POST['action'])) {
-			$this->view('Main/index');
-		} else {
+		if (isset($_POST['action'])){
+		if(isset($_POST['license'])And(isset($_POST['totalPrice']))){
+ 			$transaction = new \app\models\Main();
+ 			$transaction->license=$_POST['license'];
+ 			$transaction->location=$_POST['location'];
+ 			$transaction->duration=$_POST['duration'];
+ 			$transaction->totalPrice=$_POST['totalPrice'];
+
+ 			$data = $transaction;
+			$this->view('Main/confirmation',$data);
+                   
+                   /* $transaction->insert();
+                    $_SESSION['license'] = $_POST[license];
+$transaction->getTransaction($_POST[license]);
+if(isset($transaction->transaction_id)){
+ header('location:/Main/confirmation?transaction_id='.($transaction->transaction_id).'');}
+else{
+ header('location:/Main/index?error=Something went wrong, please try again.');}
+ */
+  } 
+
+
+
+                   /*
 			$_SESSION['license'] 	= strtoupper($_POST['license']);
 			$_SESSION['location']	= $_POST['location'];
 			$_SESSION['duration']	= $_POST['duration'];
-			header("location:/Main/confirmation");
+			*/
+
+		} else {
+
+			$this->view('Main/index');
 		}
 	}
 
 	public function confirmation() {	
 		$this->view('Main/confirmation');
 	}
+
 }
